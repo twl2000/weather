3a3a646af11133f1246aea271262e67bd29bf3da
twl2000 <twl2000@yahoo.com>
2017-04-03 10:56:27 +0100

    save local

---
+++ readme.txt
@@ -0,0 +1,28 @@
+Test script has been developed to test BBC weather AUT, based on requirement.
+
+Notes:
+1) Project "weather" created using visual studio 2015 and .Net 4.5.2
+
+2) Nuget is used to install the following packages
+* NUnit 
+* NUnit3TestAdaptor
+* Selenium.WebDriver
+* Selenium.Support
+* Selenium.WebDriver.ChromeDriver
+* Selenium.Firefox.WebDriver
+* Selenium.WebDriver.IEDriver
+
+3) Implementation on 2 C# files and 1 config file
+i - Common_Element.cs: This contains DOM elements identified on the web portal. These elements are used by the test.
+ii - weather.cs: This contains the actual test and test logic. 
+iii - app.config: This contains settings for URL, location and specified hour (i.e. 2100)
+
+4) Results (pressure retrieved from the bbc web site and difference)  are 
+i - sent to the console output
+ii - saved in file C:\temp\recordedPressure.txt
+
+5) To execute
+i - open solution using visual studio
+ii - build the solution
+iii - open Test Explorer and a test named "read_pressure" should be found.
+iv - right click to run 
---
+++ weather/Common_Elements.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace weather
+{
+    public class Common_Elements
+    {
+        //Constructor required;
+        public Common_Elements(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        //************************ Elements on page ******************************
+        //*** Main Page Title
+        [FindsBy(How = How.XPath, Using = "//a[@class='site-name']")]
+        public IWebElement lblTitle { get; set; }
+
+        //** Input location field
+        [FindsBy(How = How.Id, Using = "locator-form-search")]
+        public IWebElement txtlocation { get; set; }
+
+        //** Search submit icon
+        [FindsBy(How = How.Id, Using = "locator-form-submit")]
+        public IWebElement icnSearch { get; set; }
+
+        //** Reading link
+        [FindsBy(How = How.XPath, Using = "//a[@href='/weather/2639577']")]
+        public IWebElement linkReading { get; set; }
+
+        //** Weather page Reading title
+        [FindsBy(How = How.XPath, Using = "//span[(@class='location-name')]")]
+        public IWebElement lblReading { get; set; }
+
+        //** show table icon
+        [FindsBy(How = How.Id, Using = "detail-table-view")]
+        public IWebElement icnShowTable { get; set; }
+
+        //** weather info table
+        [FindsBy(How = How.XPath, Using = "//div[contains(@class, 'table-container selected slot-offset')]")]
+        public IWebElement tblWeather { get; set; }
+        
+        //** Date icon - tomorrow
+        [FindsBy(How = How.XPath, Using = "//a[@href='/weather/2639577?day=1']")]
+        public IWebElement icnTomorrow { get; set; }
+
+        //** Date3 icon - dateAfterTomorrow
+        [FindsBy(How = How.XPath, Using = "//a[@href='/weather/2639577?day=2']")]
+        public IWebElement icnDay3 { get; set; }
+
+        //** Find the column position containing specified hour i.e. 2100
+        public int FindHourPosition(string specifiedHour)
+        {
+            ICollection<IWebElement> rows = tblWeather.FindElements(By.XPath("//tr[@class='time']/th[contains(@class, 'value hours')]"));
+
+            int position = 0;
+            foreach (var row in rows)
+            {
+                if (row.Text == specifiedHour)
+                {
+                    return position;
+                }
+                position = position + 1;
+            }
+            return -1;
+        }
+
+        //** Read Pressure of column with hour = 2100
+        public string ReadPressure(int hr_position)
+        {
+            ICollection<IWebElement> rows = tblWeather.FindElements(By.XPath("//tr[@class='pressure']/td[contains(@class, 'value hours')]"));
+
+            int position = 0;
+            foreach (var row in rows)
+            {
+                if (position == hr_position)
+                {
+                    return row.Text;
+                }
+                position = position + 1;
+            }
+            return "null";
+        }
+    }
+}
